21450e30875304948835946f4bad0f9d335c3bee
流羽 <lycpan@foxmail.com>
2023-11-20 17:40:00 +0800

    style: 部署文件

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@
 set -e
 echo 'lycpan233.top' > CNAME
 
-push_addr=`git@github.com:lycpan233/lycpan233.github.io.git` # git提交地址，也可以手动设置，比如：push_addr=git@github.com:xugaoyi/vuepress-theme-vdoing.git
+push_addr=git@github.com:lycpan233/lycpan233.github.io.git # git提交地址，也可以手动设置，比如：push_addr=git@github.com:xugaoyi/vuepress-theme-vdoing.git
 commit_info=`git describe --all --always --long`
 dist_path=docs/.vuepress/dist # 打包生成的文件夹路径
 push_branch=deploy # 推送的分支
