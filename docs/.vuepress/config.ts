5d706da0b05543bf18e65c7751dca493fb2cab82
流羽 <lycpan@foxmail.com>
2025-03-04 17:52:49 +0800

    feat: add ICP info

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -123,8 +123,10 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
     // 页脚信息
     footer: {
       createYear: 2023, // 博客创建年份
-      copyrightInfo:
-        'Dreamer | <a href="https://github.com/xugaoyi/vuepress-theme-vdoing/blob/master/LICENSE" target="_blank">MIT License</a>', // 博客版权信息、备案信息等，支持a标签或换行标签</br>
+      copyrightInfo: `Dreamer | <a href="https://github.com/xugaoyi/vuepress-theme-vdoing/blob/master/LICENSE" target="_blank">MIT License</a>
+        </br>
+         <a href="https://beian.miit.gov.cn/" target="_blank">粤ICP备2025379918号</a>
+        `, // 博客版权信息、备案信息等，支持a标签或换行标签</br>
     },
 
     // 扩展自动生成frontmatter。（当md文件的frontmatter不存在相应的字段时将自动添加。不会覆盖已有的数据。）
